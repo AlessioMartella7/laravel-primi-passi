3eaf19bd9f91e252ee9b8996a4ad44540fa26678
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-21 16:29:30 +0200

    added dynamic data to views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = " The journey begins!";
+    return view('home',
+   [ "titolo" => $title,]
+);
 });
